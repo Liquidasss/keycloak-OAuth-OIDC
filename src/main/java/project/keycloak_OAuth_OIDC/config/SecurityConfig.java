21413087c025e2cf690b650c17fa305aefcfa5c2
Liquidasss <mustyatsaoleksandr@gmail.com>
2026-01-10 02:39:14 +0200

    roles

--- src/main/java/project/keycloak_OAuth_OIDC/config/SecurityConfig.java
+++ src/main/java/project/keycloak_OAuth_OIDC/config/SecurityConfig.java
@@ -69,7 +69,6 @@ public class SecurityConfig {
                                     .map(GrantedAuthority.class::cast))
                     .toList();
         });
-
         return converter;
     }
 
